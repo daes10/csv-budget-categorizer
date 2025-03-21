73838d63c77f3407de2cfa1e22cc1a1afa069f03
David Trachsler <tech@sub.swissdeep.ch>
2025-03-22 00:12:05 +0100

    Basic "Hello World" Main Window in tkinter

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print("Hallo es hat geklappt!")
+import tkinter as tk
+#print("Hallo es hat geklappt!")
+
+# Erstelle ein neues Fenster (main)
+main = tk.Tk()
+
+
+# place a label on the main window
+message = tk.Label(main, text="Hello, World!")
+message.pack()
+
+main.mainloop()
+
