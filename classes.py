5465ceb7d287ed47d7100555e6cadaf026747d19
David Trachsler <tech@sub.swissdeep.ch>
2025-03-27 22:42:07 +0100

    Add first lines of code to switch to OOP

---
+++ classes.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import filedialog as fd
+import json
+import os
+
+class SettingsManager:
+    """Handles loading and saving settings for the application."""
+    
+        
+
